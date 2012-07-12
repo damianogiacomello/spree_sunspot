085fc5a20d4efc1204b1ce71b5b5fa4b7dd9b74e
Damiano <damiano.giacomello@diginess.it>
2012-07-12 12:28:59 +0200

    moved initializer from config/initializer to templates. fixed bug for generator

--- lib/generators/spree_sunspot/install/install_generator.rb
+++ lib/generators/spree_sunspot/install/install_generator.rb
@@ -1,18 +1,21 @@
-#if not defined?(SunspotRails::Generators::InstallGenerator)
-#  require 'generators/sunspot_rails/install/install_generator.rb'
-#end
+if not defined?(SunspotRails::Generators::InstallGenerator)
+  require 'generators/sunspot_rails/install/install_generator.rb'
+end
 
 module SpreeSunspot
   module Generators
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path("../templates", __FILE__)
-
-      #def invoke_sunspot_rails_generator
-      #  SunspotRails::Generators::InstallGenerator.start
-      #end
+      def self.source_root
+        @source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+      end
+      
+      def invoke_sunspot_rails_generator
+        SunspotRails::Generators::InstallGenerator.start
+      end
       
       def copy_initializer_file
-        copy_file "spree_sunspot.rb", "config/initializers/#{file_name}.rb"
+        copy_file "spree_sunspot.rb", "config/initializers/spree_sunspot.rb"
       end
 
       def add_javascripts
